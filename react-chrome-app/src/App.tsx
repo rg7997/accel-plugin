c37967c814951e5aaf655474119cdd30393845b3
Rishi Garg <rishi@gkmit.co>
2024-02-19 12:06:53 +0530

    Add Tailwind CSS

--- react-chrome-app/src/App.tsx
+++ react-chrome-app/src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, ConfigProvider } from 'antd';
+import "./App.css";
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
 
     <div>
       <header>
-        <h2>Hello From Accel Plugin 1👋</h2>
+        <h2 className='text-green-500 mt-4'>Hello From Accel Plugin 1👋</h2>
         <Button type="primary" onClick={() => alert('Open')}>Open Accel Plugin</Button>
       </header>
     </div>
