22ae603562673e30dcba02dfa686db40afbd04c8
Rishi Garg <rishi@gkmit.co>
2024-02-19 11:43:36 +0530

    Add Antd Design

--- react-chrome-app/src/App.tsx
+++ react-chrome-app/src/App.tsx
@@ -1,13 +1,17 @@
+import { Button, ConfigProvider } from 'antd';
+
 function App() {
 
-  
   return (
+      <ConfigProvider theme={{ token: {  } }}>
+
     <div>
       <header>
-        <h2>Hello From Accel Plugin 👋</h2>
-        <button onClick={() => alert('Open')}>Open Accel Plugin</button>
+        <h2>Hello From Accel Plugin 1👋</h2>
+        <Button type="primary" onClick={() => alert('Open')}>Open Accel Plugin</Button>
       </header>
     </div>
+    </ConfigProvider>
   );
 }
 
